42342c3f57a1f3ef1088eb72fd26da398487d124
BrikkeH <brikke.huygen@student.kdg.be>
2020-02-08 16:25:51 +0100

    Switched lines fix

--- hangman.php
+++ hangman.php
@@ -1,7 +1,7 @@
 <?php
 include "wordlist.php";
 $rnd_word = array_rand($wordlist);
+$lettercount = strlen($rnd_word);
 echo $wordlist[$rnd_word];
-$lettercount = strlen("$rnd_word");
 echo $lettercount;
 ?>
