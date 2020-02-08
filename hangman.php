8fae9293f899a826c202d678fd941cfb21ee2901
BrikkeH <brikke.huygen@student.kdg.be>
2020-02-08 16:31:46 +0100

    Rnd word and count fix

--- hangman.php
+++ hangman.php
@@ -1,6 +1,7 @@
 <?php
 include "wordlist.php";
-$rnd_word = array_rand($wordlist);
+$rnd_word_num = array_rand($wordlist);
+$rnd_word = $wordlist[$rnd_word_num];
 $lettercount = strlen($rnd_word);
 echo $rnd_word;
 echo $lettercount;
