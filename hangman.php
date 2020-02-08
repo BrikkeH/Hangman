01c3b9455b227ee78257c8eed023e620cdb19ecf
BrikkeH <brikke.huygen@student.kdg.be>
2020-02-08 16:15:15 +0100

    Length of word test

--- hangman.php
+++ hangman.php
@@ -2,6 +2,6 @@
 include "wordlist.php";
 $rnd_word = array_rand($wordlist);
 echo $wordlist[$rnd_word];
-$lettercount = str_word_count($rnd_word);
+$lettercount = str_word_count('test');
 echo $lettercount;
 ?>
