3c55ddb1af7617a4ccfa9944306f2eaa25373099
BrikkeH <brikke.huygen@student.kdg.be>
2020-02-08 16:53:05 +0100

    Word split test

--- hangman.php
+++ hangman.php
@@ -5,4 +5,6 @@ $rnd_word = $wordlist[$rnd_word_num];
 $lettercount = strlen($rnd_word);
 echo $rnd_word;
 echo $lettercount;
+$chars = preg_split('//', $rnd_word, -1, PREG_SPLIT_NO_EMPTY);
+echo ''.print_r($chars,true).'';
 ?>
