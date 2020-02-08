83c199b6c121b1f28986bec44840930127cea8fa
BrikkeH <brikke.huygen@student.kdg.be>
2020-02-08 16:20:27 +0100

    Parenteses fix

--- hangman.php
+++ hangman.php
@@ -2,6 +2,6 @@
 include "wordlist.php";
 $rnd_word = array_rand($wordlist);
 echo $wordlist[$rnd_word];
-$lettercount = strlen('$rnd_word');
+$lettercount = strlen("$rnd_word");
 echo $lettercount;
 ?>
