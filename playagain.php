c221bf8ff0191f35734d092714b70eb77fe9049b
vincentman1 <60392350+vincentman1@users.noreply.github.com>
2020-02-12 23:37:31 +0100

    play again page

---
+++ playagain.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="stylesheet.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Lacquer|Poppins&display=swap" rel="stylesheet">
+    <title>You loose</title>
+</head>
+<body style="background-color: orange">
+    <div class="losingpage">
+        <h1>Unfortunately, you lose. </h1>
+        <button>PLAY AGAIN</button>
+    </div>
+</body>
+</html>
